eda9a4d294ec2f8589c0df51e49b7ba5c7bb0936
Fernando Avilez Tzec <diegooavilez4912@gmail.com>
2024-02-27 10:00:33 -0600

    Solo hay un nombre

---
+++ NombresJava.java
@@ -0,0 +1,11 @@
+package Paq1;
+
+public class NombresJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Mi nombre es Diego Fernando Avilez Tzec");
+	}
+
+}
